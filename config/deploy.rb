f09b569e0fff3200fb0aba1c3cbbd09427e14419
takatoshi-inoue <67253927+takatoshi-inoue@users.noreply.github.com>
2020-09-27 17:27:58 +0900

    消えたので作り直し　消えたらブラウザに表示できた

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'photo_up'
+set :application, photo_up
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:takatoshi-inoue/photo_up.git'
