64a6c17d0cef3263f49ed6ef9f63e2f8556632f8
takatoshi-inoue <67253927+takatoshi-inoue@users.noreply.github.com>
2020-11-17 00:34:39 +0900

    mis

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.14.1'
+# lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'photo_up'
+# set :application, 'photo_up'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:takatoshi-inoue/photo_up.git'
