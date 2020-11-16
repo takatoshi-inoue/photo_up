057c43ec22e36f33ae9af67b1a7fffb6cfcf8306
takatoshi-inoue <67253927+takatoshi-inoue@users.noreply.github.com>
2020-11-17 00:41:28 +0900

    d

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,10 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    AWS_ACCESS_KEY_ID_MER60 = 'i-08f38b93ae5eb1ff5'
+    AWS_SECRET_ACCESS_KEY_MER60 = 'c429d386e1ecda5f0b742c79331d4e460cbeae5d2235b892f0fc45ea166ee3b71d3b99299ed1ca70b0e82a45bcd1934e7779c5386734d25faef47dcb89dfa7d0',
+    # aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    # aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'ap-northeast-1'
   }
 
