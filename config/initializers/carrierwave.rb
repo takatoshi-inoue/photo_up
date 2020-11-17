2da6fd5dd9cc20a8d2e4df76f5aad4f63b020f6e
takatoshi-inoue <67253927+takatoshi-inoue@users.noreply.github.com>
2020-11-17 10:06:13 +0900

    c

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,39 @@
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
+
+# CarrierWave.configure do |config|
+#   config.storage = :fog
+#   config.fog_provider = 'fog/aws'
+#   config.fog_credentials = {
+#     provider: 'AWS',
+#     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+#     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+#     region: 'ap-northeast-1'
+#   }
+
+#   config.fog_directory  = 'photoupbox'
+#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/photoupbox'
+# end
+
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    AWS_ACCESS_KEY_ID_MER60 = 'i-08f38b93ae5eb1ff5'
-    AWS_SECRET_ACCESS_KEY_MER60 = 'c429d386e1ecda5f0b742c79331d4e460cbeae5d2235b892f0fc45ea166ee3b71d3b99299ed1ca70b0e82a45bcd1934e7779c5386734d25faef47dcb89dfa7d0',
-    # aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    # aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'photoupbox'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/photoupbox'
+  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+    config.storage = :file
+  elsif Rails.env.production? #本番はS3に保存する
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+# credentials下にaws_access_key_idとaws_secret_access_keyはあるよ
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 's3バケット名'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/s3バケット名'
+  end
 end
